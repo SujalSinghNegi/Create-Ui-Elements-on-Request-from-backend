8086884be4d5c1974a8ce0601877b8b2abce2cbc
Sujal Singh Negi <negisujal2007@gmail.com>
2025-04-13 17:45:48 +0530

    Initial Commit

--- app/src/main/java/com/example/createuiondemand/MainActivity.kt
+++ app/src/main/java/com/example/createuiondemand/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.createuiondemand
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+
 data class UIElement(
     val type: String,
     val label: String
@@ -54,6 +60,33 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+        val createButton:Button = findViewById<Button>(R.id.createButton)
+        val label:EditText = findViewById<EditText>(R.id.label)
+        val viewType:EditText = findViewById<EditText>(R.id.viewType)
+
+        val watcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val labelFilled = label.text.toString().isNotEmpty()
+                val typeFilled = viewType.text.toString().isNotEmpty()
+
+                if (labelFilled && typeFilled) {
+                    createButton.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.green))
+                }else{
+                    createButton.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.grey))
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        }
+
+
+        // beforeTextChanged, onTextChanged, afterTextChanged
+
+
+        label.addTextChangedListener(watcher)
+        viewType.addTextChangedListener(watcher)
+
 
 
     }
